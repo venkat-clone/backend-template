b64c1f954fda7164f4000f01bef1c64d2bc92fa7
venkat-clone <lingampally.venkey@gmail.com>
2025-09-09 23:28:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const {indexRouter} = require('./routes/index');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const {globalErrorHandler, prismaErrorHandler} = require("./utils/utils");
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', indexRouter);
+app.use(prismaErrorHandler);
+app.use(globalErrorHandler);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
